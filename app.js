cf471fc77c4c7353f4ba410eeaf21b3fb6093f06
Jeff Huang <jeffhuang@zappoint.com>
2017-02-15 16:40:17 +0800

    update express packages

--- app.js
+++ app.js
@@ -1,37 +1,35 @@
 var express = require('express');
 var atvm = require('./anonymous_tvm');
 var app = express();
+var bodyParser = require('body-parser')
 
-app.configure(function(){
-  app.set('port', process.env.PORT || 3000);
-  app.use(express.logger('dev'));
-  app.use(express.urlencoded());
-  app.use(function(req, res, next) {
-    res.contentType('application/json');
-    next();
-  });
-});
+app.set('port', process.env.PORT || 3000);
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
-app.post('/registrations', function(req, res, next) {
-  atvm.register(req.body.device_id, req.body.key, function(err, data) {
-    if (err) return next(err);
-    res.send(200, JSON.stringify({'ok':'yeah'}));
+app.post('/registrations', function (req, res, next) {
+  atvm.register(req.body.device_id, req.body.key, function (err, data) {
+    if (err) {
+      handleError(res, err);
+    }
+    res.send(200, JSON.stringify({ 'ok': 'yeah' }));
   });
 })
 
-app.get('/tokens', function(req, res, next) {
-  atvm.getToken(req.query.device_id, req.query.timestamp || 'TODO', req.query.signature || 'TODO', function(err, data) {
-    if (err) return next(err);
+app.get('/tokens', function (req, res, next) {
+  atvm.getToken(req.query.device_id, req.query.timestamp || 'TODO', req.query.signature || 'TODO', function (err, data) {
+    if (err) {
+      handleError(res, err);
+    }
     res.send(200, JSON.stringify(data));
   });
 });
 
-app.configure(function () {
-  app.use(function(err, req, res, next){
-    console.log(err.stack);
-    res.send(err.status || 500, JSON.stringify({'error':err.message}))
-  });
-});
+
+var handleError = function(res, err) {
+  console.log(err.stack);
+  res.send(err.status || 500, JSON.stringify({ 'error': err.message }))
+}
 
 app.listen(app.get('port'));
 console.log('Listening on port ' + app.get('port'));
