e0cf1be05728d3e5eed214d4080f5ce86234ee20
Jeff Huang <jeffhuang@zappoint.com>
2017-03-03 16:25:27 +0800

    refine tvm policy

--- anonymous_tvm.js
+++ anonymous_tvm.js
@@ -1,38 +1,53 @@
 var crypto = require('crypto');
 var AWS = require('aws-sdk');
-AWS.config.update({region: process.env.TVM_REGION});
-var policy = { "Version":"2012-10-17", "Statement": [ { "Effect": "Allow", "Action": "*", "Resource": "*" } ] }
-var tableName = process.env.TVM_TABLE
+AWS.config.update({ region: process.env.TVM_REGION });
+//var policy = { 'Version': '2012-10-17', 'Statement': [{ 'Effect': 'Allow', 'Action': '*', 'Resource': '*' }] }
+var policy = require('./tvm_policy.json');
+var tableName = process.env.TVM_TABLE;
 var dynamodb = new AWS.DynamoDB();
 var STS = new AWS.STS();
 
-exports.register = function(deviceID, key, callback) {
-  var item = { "device_id": {"S": deviceID}, "key": {"S": key} };
-  dynamodb.putItem({TableName:tableName, Item:item}, function(err, data) {
-    if (err) return callback(err, null);
-    callback(null, data);
-  });
+exports.register = function (deviceID, key, callback) {
+    var item = { 'device_id': { 'S': deviceID }, 'key': { 'S': key } };
+    dynamodb.getItem({ TableName: tableName, Key: { 'device_id': { 'S': deviceID } } }, function (err, data) {
+        if (err) return callback(err, null);
+        if (data) {
+            console.log('uid already registered');
+            if (key == data.Item.key.S) {
+                return callback(null, data);
+            } else {
+                console.log('uid is not matched:' + deviceID);
+                let error = new Error('register info mismatch');
+                error.status = 400;
+                return callback(error, null);
+            }
+        }
+        dynamodb.putItem({ TableName: tableName, Item: item }, function (err, data) {
+            console.log('new uid registered');
+            if (err) return callback(err, null);
+            callback(null, data);
+        });
+    });
 };
 
-exports.getToken = function(deviceID, timestamp, signature, callback) {
-  dynamodb.getItem({TableName:tableName, Key:{'device_id':{'S':deviceID}}}, function(err, data) {
-    if (err) return callback(err, null);
-    if (!data.Item) {
-      var error = new Error('item not found');
-      error.status = 400;
-      return callback(error, null);
-    }
-
-    var hash = crypto.createHmac('SHA256', data.Item.key.S).update(timestamp).digest('base64');
-    if (hash != signature) {
-      var error = new Error('signature mismatch');
-      error.status = 400;
-      return callback(error, null);
-    }
-    
-    STS.getFederationToken({'Name':deviceID,'Policy':JSON.stringify(policy)}, function (err, data) {
-      if (err) return callback(err, null);
-      callback(null, data);
+exports.getToken = function (deviceID, timestamp, signature, callback) {
+    dynamodb.getItem({ TableName: tableName, Key: { 'device_id': { 'S': deviceID } } }, function (err, data) {
+        if (err) return callback(err, null);
+        if (!data.Item) {
+            let error = new Error('item not found');
+            error.status = 400;
+            return callback(error, null);
+        }
+        var hash = crypto.createHmac('SHA256', data.Item.key.S).update(timestamp).digest('base64');
+        console.log('hash:' + hash);
+        if (hash != signature) {
+            let error = new Error('signature mismatch');
+            error.status = 400;
+            return callback(error, null);
+        }
+        STS.getFederationToken({ 'Name': deviceID, 'Policy': JSON.stringify(policy) }, function (err, data) {
+            if (err) return callback(err, null);
+            callback(null, data);
+        });
     });
-  });
 };
--- app.js
+++ app.js
@@ -1,35 +1,34 @@
 var express = require('express');
 var atvm = require('./anonymous_tvm');
 var app = express();
-var bodyParser = require('body-parser')
+var bodyParser = require('body-parser');
 
 app.set('port', process.env.PORT || 3000);
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-app.post('/registrations', function (req, res, next) {
-  atvm.register(req.body.device_id, req.body.key, function (err, data) {
-    if (err) {
-      handleError(res, err);
-    }
-    res.send(200, JSON.stringify({ 'ok': 'yeah' }));
-  });
-})
-
-app.get('/tokens', function (req, res, next) {
-  atvm.getToken(req.query.device_id, req.query.timestamp || 'TODO', req.query.signature || 'TODO', function (err, data) {
-    if (err) {
-      handleError(res, err);
-    }
-    res.send(200, JSON.stringify(data));
-  });
+app.post('/registrations', function (req, res) {
+    atvm.register(req.body.device_id, req.body.key, function (err, data) {
+        if (err) {
+            return handleError(res, err);
+        }
+        res.status(200).send(JSON.stringify({ 'result': 'ok' }));
+    });
 });
 
+app.get('/tokens', function (req, res) {
+    atvm.getToken(req.query.device_id, req.query.timestamp, req.query.signature, function (err, data) {
+        if (err) {
+            return handleError(res, err);
+        }
+        res.status(200).send(JSON.stringify(data));
+    });
+});
 
-var handleError = function(res, err) {
-  console.log(err.stack);
-  res.send(err.status || 500, JSON.stringify({ 'error': err.message }))
-}
+var handleError = function (res, err) {
+    console.log(err.stack);
+    res.status(err.status || 500).send(JSON.stringify({ 'error': err.message }));
+};
 
 app.listen(app.get('port'));
 console.log('Listening on port ' + app.get('port'));
